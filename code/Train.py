f055e47b2214a9b33cad150d8e2fe1ca60aa294d
jongja <whwhdgus94@naver.com>
2018-04-30 23:34:08 +0900

    by jongja

--- code/Train.py
+++ code/Train.py
@@ -1,4 +1,14 @@
 import tensorflow as tf
 import numpy as np
 
+## Parameters ##
+LearningRate = 0.1
+ROW = 8
+
 data = np.loadtxt("Preferance_Data.csv", delimiter = ',', dtype = np.float32)
+
+x_data = data[:,0:-1]
+y_data = data[:,[-1]]
+
+x = tf.placeholder(tf.float, shape = [None, ROW])
+y = tf.placeholder(tf.float, shape = [None, 1])
