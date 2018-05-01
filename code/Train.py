0cdf126d29095c6b850bdffdd325eb49012145b4
jongja <whwhdgus94@naver.com>
2018-05-01 16:03:05 +0900

    myungO

--- code/Train.py
+++ code/Train.py
@@ -3,12 +3,22 @@ import numpy as np
 
 ## Parameters ##
 LearningRate = 0.1
-ROW = 8
+condition = 8
+nb_classes = 14
 
-data = np.loadtxt("Preferance_Data.csv", delimiter = ',', dtype = np.float32)
+data = np.loadtxt('Preferance_Data.csv', delimiter = ',', dtype = np.float32)
 
 x_data = data[:,0:-1]
 y_data = data[:,[-1]]
 
-x = tf.placeholder(tf.float, shape = [None, ROW])
-y = tf.placeholder(tf.float, shape = [None, 1])
+x = tf.placeholder(dtype = tf.float32, shape = [None, condition)
+y = tf.placeholder(dtype = tf.int32, shape = [None, 1])
+
+y_one_hot = tf.one_hot(y, n)
+y_one_hot = tf.reshape(y_one_hot, [-1, nb_classes])
+
+w = tf.Variable(tf.random_normal([]))
+
+
+
+
